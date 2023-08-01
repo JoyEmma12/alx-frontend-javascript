c74f051c276744c1d6f1dac4334e56b3b9df5d3a
JoyEmma12 <ejoy0600@gmail.com>
2023-08-01 20:02:01 +0100

    handling multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then(([signUpRes, uploadRes]) => [
+    {
+      status: signUpRes.status,
+      value:
+        signUpRes.status === "fulfilled" ? signUpRes.value : signUpRes.reason,
+    },
+    {
+      status: uploadRes.status,
+      value:
+        uploadRes.status === "fulfilled"
+          ? uploadRes.value
+          : uploadRes.reason.message,
+    },
+  ]);
+}
