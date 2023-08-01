4023a28ac762f4160a62f3a6bd4e124ef945ce32
JoyEmma12 <ejoy0600@gmail.com>
2023-08-01 01:07:33 +0100

    promise async

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,9 +2,9 @@ export default function getFullResponseFromAPI(success) {
   // eslint-disable-next-line no-unused-vars
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: "Success" });
+      resolve({ status: 200, body: 'Success' });
     } else {
-      reject(Error("The fake API is not working currently"));
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
