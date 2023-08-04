266e17125ea8d72b1dfb68a72e8e5f3efc9e8389
JoyEmma12 <ejoy0600@gmail.com>
2023-08-04 21:48:07 +0100

    es6 data manipulation

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,17 @@
+export default function updateStudentGradeByCity(
+  studentsArray,
+  city,
+  newGrades
+) {
+  return studentsArray
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : "N/A",
+      };
+    });
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = Array.from(set).filter((value) =>
+    value.startsWith(startString)
+  );
+  const cleanedString = filteredValues
+    .map((value) => value.slice(startString.length))
+    .join('-');
+  return cleanedString;
+}
